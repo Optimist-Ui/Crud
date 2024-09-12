deb083b05f3437d8c447ba545745c451427f2fc9
Optimist-Ui <user.moosazahoormzee@gmail.com>
2024-09-12 14:41:33 +0500

    Agai3

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./assets/app";
+import App from "./assets/temp";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
